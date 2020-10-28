2b1e506a34898a965386b80a749c3acd4aa25b5f
65style <lym210119@gmail.com>
2020-10-29 04:33:25 +0800

    first commit

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,12 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:65style/65style.github.io.git master:gh-pages
+# git push -f git@github.com:65style/65style.github.io.git master:gh-pages
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+# 如果使用 travis 持续集成
+git push -f https://github.com/65style/65style.github.io.git master:gh-pages
 
 cd -
