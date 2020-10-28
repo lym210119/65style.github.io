a4e7d1cc712417cdcc3fee09e1028686bbdf9bbf
65style <lym210119@gmail.com>
2020-10-28 17:18:28 +0800

    first commit

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,13 @@ cd .vuepress/dist
 # echo 'www.example.com' > CNAME
 
 git init
+git remote add origin https://github.com/65style/65style.github.io.git
+git checkout gh-pages
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:65style/65style.github.io.git gh-pages
+git push -f origin gh-pages
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
